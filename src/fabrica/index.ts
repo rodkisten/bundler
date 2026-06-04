dc98b9bdabdab0e146c82f4be43b1b3ef0dde38c
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-04 15:12:04 -0300

    Add files via upload

--- src/fabrica/index.ts
+++ src/fabrica/index.ts
@@ -1,44 +1,11 @@
-export { createFabricaApi } from "./public-api";
-export type { FabricaApi } from "./public-api";
-export type {
-  Cleanup,
-  Component,
-  ComponentContext,
-  DebugSnapshot,
-  Directive,
-  DomBag,
-  InstallOptions,
-  RawHtml,
-  RenderValue,
-  RepeatContext,
-  Signal,
-} from "./types";
-
-export { $ } from "./bag";
-export { css } from "./css";
-export { component } from "./component";
-export { debug, setDebug } from "./debug";
-export { classMap, ref, repeat, styleMap, when } from "./directives";
-export { html, mount, render } from "./dom";
-export { rawHtml } from "./raw";
-export { batch, computed, effect, memo, onCleanup, signal, untrack } from "./reactivity";
-
-import { createFabricaApi } from "./public-api";
-
-/** Default singleton API. */
-const Fabrica = createFabricaApi();
-
-if (typeof globalThis !== "undefined") {
-  Fabrica.install();
+export function html(strings: TemplateStringsArray, ...values: unknown[]): DocumentFragment {
+  const template = document.createElement("template");
+  template.innerHTML = strings.reduce((output, chunk, index) => `${output}${chunk}${String(values[index] ?? "")}`, "");
+  return template.content.cloneNode(true) as DocumentFragment;
 }
-
-export default Fabrica;
-
-declare global {
-  // eslint-disable-next-line no-var
-  var Fabrica: import("./public-api").FabricaApi | undefined;
-  // eslint-disable-next-line no-var
-  var $: unknown;
-  // eslint-disable-next-line no-var
-  var $el: unknown;
+export function render(container: Element, value: Node): () => void {
+  container.replaceChildren(value);
+  return () => container.replaceChildren();
 }
+export const version = "1.0.0";
+export default { html, render, version };
