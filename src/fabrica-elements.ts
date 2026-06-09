04e22c8227e1f964c74f078f2c36dd4c1d02c35f
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-09 18:40:01 -0300

    Create fabrica-elements.ts

---
+++ src/fabrica-elements.ts
@@ -0,0 +1,8 @@
+/**
+ * @tool Fabrica Elements
+ * @global FabricaElements
+ * @package fabrica-elements
+ * @tags elements components adapters dom styled userscripts
+ * @description Shared element/component factory layer used by Cipó and Fábrica.
+ */
+export * from './fabrica-elements/index'
