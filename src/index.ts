8219fc885e2147c3416a6cf07e945ef7dd829982
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-19 19:29:19 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@
  * @tags namespace browser userscripts
  * @description Umbrella browser namespace that can aggregate your root tools when you want one global.
  */
-export * as Fabrica from "./fabrica";
+export * as Broto from "./broto";
 export * as Cipo from "./cipo";
+export * as Fabrica from "./fabrica";
+export * as FabricaElements from "./fabrica-elements";
 export * as Seiva from "./seiva-state";
