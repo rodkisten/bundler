e970543ac4d81a2cec7e50fb5ecd2e3bca7f3748
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-23 09:19:05 -0300

    Update vitest.config.ts

--- src/cipo/vitest.config.ts
+++ src/cipo/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
   },
   reporters: [
+       'verbose',
       ['github-actions', {
         jobSummary: {
           outputPath: '/home/runner/jobs/summary/step',
