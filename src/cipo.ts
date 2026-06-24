013b3e0f7052e40222189a3f292a019a4c42c6c5
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-24 14:36:06 -0300

    activate Cipó CSS safety layer

--- src/cipo.ts
+++ src/cipo.ts
@@ -5,4 +5,29 @@
  * @tags css jit atomic userscripts
  * @description Browser-first atomic CSS runtime and semantic CSS DSL bundled as a standalone browser global.
  */
-export * from "./cipo/src/index";
+import { cipo as coreCipo, styled as coreStyled } from './cipo/src/index'
+import { hydrateDocumentStartStyles } from './cipo/src/document-start-hydration'
+import { installNativePropertyGuards } from './cipo/src/native-property-guards'
+import { safeAtomic } from './cipo/src/safe-atomic'
+import { safeCss } from './cipo/src/safe-css'
+import { safeInline } from './cipo/src/safe-inline'
+import { safeSheet } from './cipo/src/safe-sheet'
+
+installNativePropertyGuards()
+hydrateDocumentStartStyles()
+Object.assign(coreCipo, {
+  css: safeCss,
+  atomic: safeAtomic,
+  inline: safeInline,
+  sheet: safeSheet,
+})
+
+export * from './cipo/src/index'
+export {
+  coreCipo as cipo,
+  coreStyled as styled,
+  safeAtomic as atomic,
+  safeCss as css,
+  safeInline as inline,
+  safeSheet as sheet,
+}
