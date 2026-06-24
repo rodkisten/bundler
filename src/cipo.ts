b81d24f7b319a3ed2f0aab337e0f52be083a8c19
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-24 16:56:40 -0300

    hydrate buffered Cipó theme styles at document-start

--- src/cipo.ts
+++ src/cipo.ts
@@ -5,4 +5,8 @@
  * @tags css jit atomic userscripts
  * @description Browser-first atomic CSS runtime and semantic CSS DSL bundled as a standalone browser global.
  */
+import { hydrateDocumentStartStyles } from './cipo/src/document-start-hydration'
+
+hydrateDocumentStartStyles()
+
 export * from "./cipo/src/index";
