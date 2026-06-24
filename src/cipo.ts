8c09cb33126fe8b09f297a3d5df2c02ebfbe4f12
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-24 17:58:55 -0300

    fix(cipo): preserve current browser entrypoint

--- src/cipo.ts
+++ src/cipo.ts
@@ -5,8 +5,24 @@
  * @tags css jit atomic userscripts
  * @description Browser-first atomic CSS runtime and semantic CSS DSL bundled as a standalone browser global.
  */
+import {
+  atomic as coreAtomic,
+  cipo as coreCipo,
+  css as coreCss,
+  inline as coreInline,
+  sheet as coreSheet,
+  styled as coreStyled,
+} from './cipo/src/index'
 import { hydrateDocumentStartStyles } from './cipo/src/document-start-hydration'
 
 hydrateDocumentStartStyles()
 
-export * from "./cipo/src/index";
+export * from './cipo/src/index'
+export {
+  coreAtomic as atomic,
+  coreCipo as cipo,
+  coreCss as css,
+  coreInline as inline,
+  coreSheet as sheet,
+  coreStyled as styled,
+}
