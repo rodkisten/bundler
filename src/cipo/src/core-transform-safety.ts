04dd934753fc04d3f479a4de8655146f0d0d454d
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-24 16:55:29 -0300

    install native property guards in core transform

--- src/cipo/src/core-transform-safety.ts
+++ src/cipo/src/core-transform-safety.ts
@@ -1,10 +1,18 @@
 import { normalizeCompactRuntimeBlocks } from './compact-block-safety'
 import { expandCoreSizeCalls } from './core-size-safety'
+import { installNativePropertyGuards } from './native-property-guards'
 import { protectNativeSlashes } from './native-slash-protection'
 import { restoreNativeSlash } from './restore-native-slash'
 import { joinNestedSelectorLists } from './selector-list-safety'
 
+let nativeGuardsInstalled = false
+
 export function prepareCoreCssInput(input: string): string {
+  if (!nativeGuardsInstalled) {
+    installNativePropertyGuards()
+    nativeGuardsInstalled = true
+  }
+
   return protectNativeSlashes(
     expandCoreSizeCalls(normalizeCompactRuntimeBlocks(input)),
   )
