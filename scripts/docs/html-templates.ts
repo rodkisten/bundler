76fbc3aaafe553c6ac7a37fd3f8568daf3c84ee8
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-27 20:38:10 -0300

    Create html-templates.ts

---
+++ scripts/docs/html-templates.ts
@@ -0,0 +1,11 @@
+export { createCodePageHtml } from "./code-page";
+export { createIndexHtml } from "./landing-page";
+export { createMarkdownPageHtml } from "./markdown-page";
+
+export type {
+  CodePageInput,
+  GeneratedCodePage,
+  GeneratedDoc,
+  LandingInput,
+  MarkdownPageInput,
+} from "./doc-types";
