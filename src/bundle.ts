21fbcca5dad23bbf3016c4454f5f12d0226abab3
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-29 10:43:46 -0300

    Create bundle.ts

---
+++ src/bundle.ts
@@ -0,0 +1,11 @@
+/**
+ * @tool Bundle
+ * @global Rod
+ * @package bundlr
+ * @tags namespace browser userscripts
+ * @description Umbrella browser namespace that can aggregate your root tools when you want one global.
+ */
+export * as Broto from "./broto";
+export * as Cipo from "./cipo";
+export * as Fabrica from "./fabrica";
+export * as FabricaElements from "./fabrica-elements";
