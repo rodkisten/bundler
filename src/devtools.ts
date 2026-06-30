9ec9e4fe133f490e56342ddde0e76819ba19337b
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-30 12:31:17 -0300

    Create devtools.ts

---
+++ src/devtools.ts
@@ -0,0 +1,9 @@
+/**
+ * @tool DevTools (eruda fork)
+ * @global DevTools
+ * @package devtools
+ * @tags devtools browser mobile
+ * @description Fork of Eruda using Mata/Folclore tools.
+ */
+export * from "./devtools/index";
+export { default } from "./devtools/index";
