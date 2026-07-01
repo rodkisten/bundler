206814e370fcbd5dee96655667d9cd63d113d468
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-01 17:17:19 -0300

    fix devtools runtime styled export

--- src/devtools/components/runtime.ts
+++ src/devtools/components/runtime.ts
@@ -1,5 +1,5 @@
 import { store } from "../../broto";
-import { createCompiledStyled, createStyled } from "../../cipo";
+import { createStyled } from "../../cipo";
 import { createFabrica } from "../../fabrica";
 import type { Component, RenderValue } from "../../fabrica";
 
@@ -29,7 +29,7 @@ export const onUnmount = devtoolsFabrica.onUnmount;
 export const onDispose = devtoolsFabrica.onDispose;
 
 export const styled = createStyled({ fabrica: devtoolsFabrica });
-export const compiledStyled = createCompiledStyled({ fabrica: devtoolsFabrica });
+export const compiledStyled = styled;
 
 export interface DevtoolsUiState extends Record<string, unknown> {
   shell: { inline: boolean; mounted: boolean };
