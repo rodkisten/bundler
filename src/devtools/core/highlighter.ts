402dc8b7c3d068144c73d6f6daa089806f508c78
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-13 17:10:14 -0300

    Update highlighter.ts

--- src/devtools/core/highlighter.ts
+++ src/devtools/core/highlighter.ts
@@ -65,7 +65,7 @@ export class ElementHighlighter {
   inset: 0;
   width: 100%;
   height: 100%;
-  z-index: 2147483000;
+  z-index: 5;
   pointer-events: none !important;
   user-select: none;
   overflow: hidden;
