d9e90385488ae3e0ff40b6eca4c66b18cd950ad1
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-14 10:27:30 -0300

    Create maquina.ts

---
+++ src/maquina.ts
@@ -0,0 +1,9 @@
+/**
+ * @tool Maquina
+ * @global Maquina
+ * @package maquina
+ * @tags code editor codemirror
+ * @description A tiny, themeable, Safari-safe editor with syntax color, live suggestions and zero heavyweight editor dependency
+ */
+export * from "./maquina/index";
+export { default } from "./maquina/index";
