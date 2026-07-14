d45aca509593c8ebbb3431241c536124cdf309c0
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-14 18:07:27 -0300

    Delete maquina.ts

--- maquina.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @tool Maquina
- * @global Maquina
- * @package maquina
- * @tags code editor codemirror
- * @description A tiny, themeable, Safari-safe editor with syntax color, live suggestions and zero heavyweight editor dependency
- */
-export * from "./maquina/index";
-export { default } from "./maquina/index";
