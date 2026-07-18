c887684485aa6f5841a4ca5008dded75b64a781b
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-18 18:06:53 -0300

    Delete cipo/safe-template.ts

--- cipo/safe-template.ts
+++
@@ -1,7 +0,0 @@
-import { normalizeCompactRuntimeBlocks } from '@rodkisten/cipo/compact-block-safety'
-import { protectNativeSlashes } from '@rodkisten/cipo/native-slash-protection'
-import { joinNestedSelectorLists } from '@rodkisten/cipo/selector-list-safety'
-
-export function normalizeTemplateChunk(value: string): string {
-  return protectNativeSlashes(joinNestedSelectorLists(normalizeCompactRuntimeBlocks(value)))
-}
