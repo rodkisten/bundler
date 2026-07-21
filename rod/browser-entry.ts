143542a33fc676af30e943e7d1411110802622c7
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-21 10:05:25 -0300

    Update browser-entry.ts

--- rod/browser-entry.ts
+++ rod/browser-entry.ts
@@ -11,4 +11,3 @@ export * as Fabrica from "@rodkisten/fabrica";
 export * as FabricaElements from "@rodkisten/fabrica-elements";
 export * as Maquina from "@rodkisten/maquina";
 export * as Nascente from "@rodkisten/nascente";
-export { api as DevTools } from "@rodkisten/devtools";
