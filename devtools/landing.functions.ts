b3ca1285a9475f23410837996e5465c2ad6a71d4
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-07-25 07:32:50 -0300

    Update landing.functions.ts

--- devtools/landing.functions.ts
+++ devtools/landing.functions.ts
@@ -7,7 +7,8 @@ import { filterArray, includesArray } from "@rodkisten/nascente";
 
 export const DEFAULT_DEVTOOLS_BUNDLE_URL =
   "https://rod.migos.club/bundler/devtools.iife.js";
-export const DEFAULT_ERUDA_BUNDLE_URL = "https://cdn.jsdelivr.net/npm/eruda@latest/eruda.js";
+//export const DEFAULT_ERUDA_BUNDLE_URL = "https://cdn.jsdelivr.net/npm/eruda@latest/eruda.js";
+export const DEFAULT_ERUDA_BUNDLE_URL = "https://rod.migos.club/roderuda/roderuda/roderuda.js";
 
 export const LANDING_PANEL_NAMES = [
   "console",
